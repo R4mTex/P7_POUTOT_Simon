0053270d13d380cfdc86ed51a69413fa1ea70c22
R4mTex <poutots@gmail.com>
2022-08-17 18:36:15 +0200

    add front-end fonctionnalities

--- GrandPyApp/static/js/ajax_request.js
+++ GrandPyApp/static/js/ajax_request.js
@@ -1,9 +1,20 @@
 function script(){
-	change_text_send_button()
-	send_request()
-	show_bloc_historical()
-	show_input_historical()
+	if($("#form_input_question").val() != ""){
+		change_text_form()
+		send_request()
+		test_rotation()
+		show_history_bloc()
+		show_history_input()
+	}
+	else{
+	}
 }
+
+$(document).on('keypress', function(event) {
+    if(event.which == 13) {
+        script()
+    }
+});
 
 function send_request(){
 	$.ajax({
@@ -19,28 +30,56 @@ function send_request(){
 	});
 };
 
-function change_text_send_button(){
-	if($("#form_send_button").val("Prés à envoyer")) {
-		$("#form_send_button").val("Envoyé");
+function change_text_form(){
+	const btn = $("#form_send_button")
+	const input = $("#form_input_question")
+	if(btn.val("Prés à envoyer")){
+		btn.val("Envoyé");
 	}
-	else {
-		$("#form_send_button").val("Prés à envoyer");
+	else{
+		btn.val("Prés à envoyer");
 	}
+	setTimeout(() => {
+		btn.val("Prés à envoyer")
+		input.val("")
+	}, 3000);
 };
 
-function show_bloc_historical(){
-	if($("#result_question").css("display", "none")) {
-		$("#result_question").css("display", "block");
+function show_history_bloc(){
+    const res = $("#result_question")
+	if(res.css("display", "none")){
+		res.css("display", "block");
 	}
-	else {
-		$("#result_question").css("display", "none");
+	else{
+		res.css("display", "none");
 	}
 };
 
 const input_values = []
+i = -1
 
-function show_input_historical(){
+function show_history_input(){
 	input_values.push($("#form_input_question").val());
-	$("#asked_question").val(input_values);
-	console.log(input_values);
+	i += 1
+	$("#asked_question").append('<li class="list-unstyled">- ' + input_values[i] + '</li>');
 };
+
+function test_rotation(){
+	const icon = $("#loading_icon")
+	if(icon.css("display", "none")){
+		icon.css("display", "block");
+	}
+	else{
+		icon.css("display", "none");
+	}
+	if(icon.css("animation-play-state", "paused")){
+		icon.css("animation-play-state", "running");
+	}
+	else{
+		icon.css("animation-play-state", "paused");
+	}
+	setTimeout(() => {
+	icon.css("animation-play-state", "paused");
+	icon.css("display", "none");
+    }, 3000);
+}
