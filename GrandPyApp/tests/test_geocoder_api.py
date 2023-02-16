24440c26159cae601b05f60b18d82d7547ae305c
R4mTex <poutots@gmail.com>
2023-02-16 17:49:52 +0100

    add modifications on the geocoder_api test

--- GrandPyApp/tests/test_geocoder_api.py
+++ GrandPyApp/tests/test_geocoder_api.py
@@ -19,7 +19,85 @@ def test_get_information_from_the_request(mock_requests_get):
     sut = Geocoder(query='')
 
     mock_requests_get.return_value = mock.Mock(
-        name='mock response', **{'status_code': 200, 'json.return_value': {'status': 'OK', 'results': ''}}
-        )
+        name='mock response', **{"status_code": 200,
+                                 "json.return_value": {
+                                    "status": "OK",
+                                    "results": [
+                                        {
+                                            "address_components": [
+                                                {
+                                                    "long_name": "Paris",
+                                                    "short_name": "Paris",
+                                                    "types": [
+                                                        "locality",
+                                                        "political"
+                                                    ]
+                                                },
+                                                {
+                                                    "long_name": "Paris",
+                                                    "short_name": "Paris",
+                                                    "types": [
+                                                        "administrative_area_level_2",
+                                                        "political"
+                                                    ]
+                                                },
+                                                {
+                                                    "long_name": "Île-de-France",
+                                                    "short_name": "IDF",
+                                                    "types": [
+                                                        "administrative_area_level_1",
+                                                        "political"
+                                                    ]
+                                                },
+                                                {
+                                                    "long_name": "France",
+                                                    "short_name": "FR",
+                                                    "types": [
+                                                        "country",
+                                                        "political"
+                                                    ]
+                                                }
+                                            ],
+                                            "formatted_address": "Paris, France",
+                                            "geometry": {
+                                                "bounds": {
+                                                    "northeast": {
+                                                        "lat": 48.9021475,
+                                                        "lng": 2.4698509
+                                                    },
+                                                    "southwest": {
+                                                        "lat": 48.8155622,
+                                                        "lng": 2.2242191
+                                                    }
+                                                },
+                                                "location": {
+                                                    "lat": 48.856614,
+                                                    "lng": 2.3522219
+                                                },
+                                                "location_type": "APPROXIMATE",
+                                                "viewport": {
+                                                    "northeast": {
+                                                        "lat": 48.9021475,
+                                                        "lng": 2.4698509
+                                                    },
+                                                    "southwest": {
+                                                        "lat": 48.8155622,
+                                                        "lng": 2.2242191
+                                                    }
+                                                }
+                                            },
+                                            "partial_match": "true",
+                                            "place_id": "ChIJD7fiBh9u5kcRYJSMaMOCCwQ",
+                                            "types": [
+                                                "locality",
+                                                "political"
+                                                ]
+                                        }
+                                    ]
+                                }
+                            }
+                        )
 
-    assert sut.get_information_from_the_request() == mock_requests_get.return_value
+    assert sut.get_information_from_the_request() == {'long_name': 'Paris',
+                                                                   'status': 'OK', 'lat': 48.856614, 'lng': 2.3522219,
+                                                                   'place_id': ['ChIJD7fiBh9u5kcRYJSMaMOCCwQ']}
